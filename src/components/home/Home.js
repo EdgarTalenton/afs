0efd14ad40146880bd1a5db4939c4623e172142b
blikroy <elroyvidal@gmai.com>
2022-10-20 02:18:35 -0300

    home retoques

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,10 +5,59 @@ const Home = () => {
     return(
         <>
         <div className="container-home-fluid">
+            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img src={Principal} className="d-block w-100" alt="..." />
+                    <div className="boxcarousel">
+                        <h5>First slide label</h5>
+                        <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                    </div>
+                    <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1419064642531-e575728395f2?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1400" className="d-block w-100" alt="..." />
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                    </div>
+                    <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1445280471656-618bf9abcfe0?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1400" className="d-block w-100" alt="..." />
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+            <div className="secundarioHome">
+                <div className="flexbox">
+                    <div className="box">Nosotros</div>
+                    <div className="box">Proyectos</div>
+                    <div className="box">Novedades</div>
+                </div>
+            </div>
+            {/*
             <div className="principalHome" style={{backgroundImage: `url(${Principal})`, backgroundSize: 'cover'}}>
                 <div className="degradeBackground"></div>
-                <h1 className='titulo-principal m-0'>Más de 40 años en el desarrollo <br /> de emprendimientos para la salud.</h1>
+                <div className="container">
+                    <h1 className='titulo-principal m-0'>Más de 40 años en el desarrollo <br /> de emprendimientos para la salud.</h1>
+                </div>
             </div>
+            */}
         </div>
         </>
     );
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,21 +4,24 @@ import './NavBar.css';
 const NavBar = () => {
     return(
         <>
-        <div className="container-fluid">
             <header>
-                <div className="logo">
-                    <Link to='/'><img src={logo} alt="AFS Arquitectos" /></Link>
+                <div className="container">
+                    <div className="flexbox">
+                        <div className="logo">
+                            <Link to='/'><img src={logo} alt="AFS Arquitectos" width="220" /></Link>
+                        </div>
+                        <nav>
+                            <ul>
+                                <li>Nosotros</li>
+                                <li>Proyectos</li>
+                                <li>Novedades</li>
+                                <li>Contacto</li>
+                                <li><i className="fab fa-linkedin"></i></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-                <nav>
-                    <ul>
-                        <li>Nosotros</li>
-                        <li>Proyectos</li>
-                        <li>Novedades</li>
-                        <li>Contacto</li>
-                    </ul>
-                </nav>
             </header>
-        </div>
         </>
     )
 }
