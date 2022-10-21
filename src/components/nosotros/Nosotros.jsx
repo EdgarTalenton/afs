5b8aaf0060098e58f0c0bc0ef3ce7fce133536dc
blikroy <elroyvidal@gmai.com>
2022-10-21 17:20:03 -0300

    deploy GitHub

--- src/components/nosotros/Nosotros.jsx
+++ src/components/nosotros/Nosotros.jsx
@@ -1,18 +1,19 @@
 import './Nosotros.css';
 import grupal from '../../utils/img/nosotros/A-Nosotros-grupal-boceto.jpg';
+import { Link } from 'react-router-dom';
 
 const Nosotros = () => {
     return(
         <>
         <div className="container padd-global">
             <ul className='subMenuNosotros'>
-                <li>Socios</li>
+                <li><Link to='/'>Socios</Link></li>
                 <li>/</li>
-                <li>Colaboradores</li>
+                <li><Link to='/'>Colaboradores</Link></li>
                 <li>/</li>
-                <li>Consultores</li>
+                <li><Link to='/'>Consultores</Link></li>
                 <li>/</li>
-                <li>Competencia</li>
+                <li><Link to='/'>Competencia</Link></li>
             </ul>
             <p className='parrafo'>Con sede en Buenos Aires, el estudio AFS Arquitectos está compuesto por capacitados profesionales entrenados en el trabajo interdisciplinario y participativo que se ha desempeñado en los ámbitos público y privado. AFS Arquitectos se especializa en emprendimientos para la Salud. La permanente capacitación y actualización de su equipo de trabajo y el número de proyectos y edificios realizados lo ubican entre los equipos líderes en programación, diseño y construcción de edificios en Argentina.</p>
             <img src={grupal} alt="" className='w-100' />
