98930e7f75896381ab10a15690a5ae2b45f3ac2b
blikroy <elroyvidal@gmai.com>
2022-10-21 12:36:33 -0300

    home implementado

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,13 @@
-import Principal from '../../utils/img/home-principal.jpg';
+import slider1 from '../../utils/img/slider-home/1-UP-San-Martin-S.jpg';
+import slider2 from '../../utils/img/slider-home/2-Hospital-Aleman-8-S.jpg';
+import slider3 from '../../utils/img/slider-home/3-Finochietto--Acceso-Cordoba-S.jpg';
+import slider4 from '../../utils/img/slider-home/4-ALCLA-Fachada-2-S.jpg';
+import slider5 from '../../utils/img/slider-home/5-ALCLA-Internacion-S.jpg';
+import slider6 from '../../utils/img/slider-home/6-Finochietto-Cafeteria-S.jpg';
+import boton1 from '../../utils/img/slider-home/boton1.jpeg';
+import boton2 from '../../utils/img/slider-home/boton2.png';
+import boton3 from '../../utils/img/slider-home/boton3.jpeg';
+
 import './Home.css';
 
 const Home = () => {
@@ -10,28 +19,52 @@ const Home = () => {
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
                 </div>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                    <img src={Principal} className="d-block w-100" alt="..." />
-                    <div className="boxcarousel">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1419064642531-e575728395f2?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1400" className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
+                        <img src={slider1} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
                     </div>
                     <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1445280471656-618bf9abcfe0?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1400" className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
+                        <img src={slider2} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
                     </div>
+                    <div className="carousel-item">
+                        <img src={slider3} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={slider4} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={slider5} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={slider6} className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -45,19 +78,20 @@ const Home = () => {
             </div>
             <div className="secundarioHome">
                 <div className="flexbox">
-                    <div className="box">Somos</div>
-                    <div className="box">Proyectos</div>
-                    <div className="box">Novedades</div>
+                    <div className="box" style={{backgroundImage: `url(${boton1})`, backgroundSize: 'cover', backgroundPosition: 'center',backgroundRepeat: 'no-repeat'}}>
+                        <div className="boxFondo"></div>
+                        <p>Nosotros</p>
+                    </div>
+                    <div className="box" style={{backgroundImage: `url(${boton2})`, backgroundSize: 'cover', backgroundPosition: 'center',backgroundRepeat: 'no-repeat'}}>
+                        <div className="boxFondo"></div>
+                        <p>Proyectos</p> 
+                    </div>
+                    <div className="box" style={{backgroundImage: `url(${boton3})`, backgroundSize: 'cover', backgroundPosition: 'center',backgroundRepeat: 'no-repeat'}}>
+                        <div className="boxFondo"></div>
+                        <p>Novedades</p>
+                    </div>
                 </div>
             </div>
-            {/*
-            <div className="principalHome" style={{backgroundImage: `url(${Principal})`, backgroundSize: 'cover'}}>
-                <div className="degradeBackground"></div>
-                <div className="container">
-                    <h1 className='titulo-principal m-0'>Más de 40 años en el desarrollo <br /> de emprendimientos para la salud.</h1>
-                </div>
-            </div>
-            */}
         </div>
         </>
     );
